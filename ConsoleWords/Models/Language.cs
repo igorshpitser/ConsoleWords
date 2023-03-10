a15e22084b0d9430cfdcf8c201f9b24f46cbcf65
Ihor Shpitser <ihor.shpitser@lnu.edu.ua>
2023-03-10 11:59:36 +0200

    recert added description

--- ConsoleWords/Models/Language.cs
+++ ConsoleWords/Models/Language.cs
@@ -11,15 +11,12 @@ namespace words.Models
         public Guid Id { get; set; }
         public string Title { get; set; }
         public List<Word> Words { get; set; }
-        public String Description { get; set; }
-
-        public Language(Guid id, string title, List<Word> words, string description, List<Group> groups)
+        public Language(Guid id, string title, List<Word> words)
         {
             Id = id;
             Title = title;
             Words = words;
-			Description = description;
-            Groups = groups;
+
         }
 
         public double getAverageLevel()
